e6efca62964d27b651f8fa93932bfab84f126808
Desh Raj <r.desh26@gmail.com>
2021-11-13 14:32:51 -0500

    directory restructure

--- path.sh
+++ path.sh
@@ -1,5 +1,6 @@
 # Print immediately
 export PYTHONUNBUFFERED=1
+export PYTHONPATH=${PYTHONPATH}:`pwd`/scripts/python
 
 # Activate environment
 . /home/draj/anaconda3/etc/profile.d/conda.sh && conda deactivate && conda activate ovad
